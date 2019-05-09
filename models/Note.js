09333aa89b6a7b85cc213fc9c7f53c3c0751e865
Joseph Serrato <joseph.serrato@gmail.com>
2019-05-08 19:48:29 -0700

    almost done hw assignment

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+
+var NoteSchema = new Schema({
+  // `title` is of type String
+  title: String,
+  // `body` is of type String
+  body: String
+});
+
+
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
